c81be18545f4fe95c130db765a525f4a1810b39b
서버 <jinh574@naver.com>
2016-05-22 14:44:13 +0900

    # Git 자동배포 연동

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/webhook', function(req, res, next) {
+	if(req.header('x-gitlab-token') === "starnight") {
+		exec('.deploy.sh');
+		res.sendStatus(200)
+	}
+	else {
+		res.sendStatus(404);
+	}
+});
+
 module.exports = router;
