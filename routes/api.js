94ae7dc92f25c835dcca538e8a82c0c986ebfbfb
서버 <jinh574@naver.com>
2016-05-27 06:32:29 +0900

    # 프로토타입 우선순위 변화, 사고공사 이벤트 전

--- routes/api.js
+++ routes/api.js
@@ -8,4 +8,52 @@ router.get('/', function(req, res, next) {
     res.send("Json: " + xml.getJson());
 });
 
+router.get('/ridefilter', function(req, res, next) {
+	 var parser = new jsParser("http://dev.odsay.com/Denny_test/appleTree/v1/0/Path/PathSearch_Exit.asp?Output=json&Echo=yes&Output=json&SX=127.0414533&SY=37.5551612&EX=126.9401193&EY=37.5597722&OPT=0&svcID=56f14f5c5ed77cdf26fec5f852c54abe");
+	 var peopleParser = new Array;;
+	 var time =['ONE_RIDE_NUM', 'TWO_RIDE_NUM', 'THREE_RIDE_NUM', 'FOUR_RIDE_NUM', 'FIVE_RIDE_NUM', 'SIX_RIDE_NUM', 'SEVEN_RIDE_NUM', 'EIGHT_RIDE_NUM', 'NINE_RIDE_NUM', 'TEN_RIDE_NUM', 'ELEVEN_RIDE_NUM', 'TWELVE_RIDE_NUM', 'THIRTEEN_RIDE_NUM', 'FOURTEEN_RIDE_NUM', 'FIFTEEN_RIDE_NUM', 'SIXTEEN_RIDE_NUM', 'SEVENTEEN_RIDE_NUM', 'EIGHTEEN_RIDE_NUM', 'NINETEEN_RIDE_NUM', 'TWENTY_RIDE_NUM', 'TWENTY_ONE_RIDE_NUM', 'TWENTY_TWO_RIDE_NUM', 'TWENTY_THREE_RIDE_NUM', 'MIDNIGHT_RIDE_NUM'];
+	 var path = parser.getJson().result.path;
+	 var result = new Array;
+	 
+	 for(var i = 0; i < path.length; i++)
+	 {
+		 if(path[i].pathType == 1)
+		 {
+			 for(var j = 0; j < path[i].subPath.length; j++)
+			 {
+				 if(path[i].subPath[j].trafficType == 1)
+				 {
+					 peopleParser = new jsParser("http://openapi.seoul.go.kr:8088/645a4243536a6b6237354c666d586e/json/CardSubwayTime/1/5/201603/"  + encodeURI(encodeURIComponent(path[i].subPath[j].lane[0].subwayCode) +"호선/")+encodeURIComponent(path[i].subPath[j].startName));
+					 console.log("kb s", peopleParser.getSubwayPeople(time[7]));
+					 if(peopleParser.getSubwayPeople(time[7]) == "passed")
+					 {
+						 result.push(path[i]);
+					 }
+					 break;
+				 }
+			 }
+			
+		 }
+		 if(path[i].pathType == 2)
+		 {
+			 for(var j = 0; j < path[i].subPath.length; j++)
+			 {
+				 if(path[i].subPath[j].trafficType == 2)
+				 {
+					 peopleParser = new jsParser("http://openapi.seoul.go.kr:8088/645a4243536a6b6237354c666d586e/json/CardBusTimeNew/1/100/201603/"  + encodeURI(encodeURIComponent(path[i].subPath[j].lane[0].busNo)));
+					 console.log("kb b", peopleParser.getBusPeople(path[i].subPath[j].startName, time[7]));
+					 if(peopleParser.getBusPeople(path[i].subPath[j].startName, time[7]) == "passed")
+					 {
+						 result.push(path[i]);
+					 }
+					 break;
+				 } 
+			 }
+
+		 }
+	 }
+	 
+	 res.send(result);
+});
+
 module.exports = router;
--- routes/sg.js
+++ routes/sg.js
@@ -3,21 +3,55 @@ var router = express.Router();
 var testing = require('../lib/general');
 var sg_func = require('../lib/sgfunction');
 
-router.get('/', function(req, res) 
+router.get('/search', function(req, res) 
 {
-	var parser = new testing ("http://dev.odsay.com/Denny_test/appleTree/v1/0/Path/PathSearch_Exit.asp?Echo=yes&Output=json&SX=127.0414533&SY=37.5551612&EX=126.9401193&EY=37.5597722&OPT=0&svcID=56f14f5c5ed77cdf26fec5f852c54abe");
+	//큰게 lng
+	var srcLat = req.params.srcLat;
+	var srcLng = req.params.srcLng;
+	var dstLat = req.params.dstLat;
+	var dstLng = req.params.dstLng;
+	console.log(srcl)
+	var parser = new testing ("http://dev.odsay.com/Denny_test/appleTree/v1/0/Path/PathSearch_Exit.asp?Echo=yes&Output=json&SX="+srcLng+"&SY="+srcLat+"&EX="+dstLng+"&EY="+dstLat+"&OPT=0&svcID=56f14f5c5ed77cdf26fec5f852c54abe");
 	var obj=parser.getJson();
-	sg_func.searchRoute(obj);
-	res.send(obj);
+	res.send(sg_func.searchddRoute(obj));
 });
 
-router.get('/map', function(req, res) 
+router.get('/path',function(req,res)
 {
-	//var parser = new testing ("http://openapi.map.naver.com/openapi/naverMap.naver?ver=2.0&key=qsXajb2Y58r6evyasO9j");
-	//var obj=parser.getJson();
-	//sg_func.getMap(obj);
-	//res.send(obj);
-	 res.render('sg', { title: '웹기술및응용', name : '몽둥길' });
+	var srcLat = req.query.srcLat;
+	var srcLng = req.query.srcLng;
+	var dstLat = req.query.dstLat;
+	var dstLng = req.query.dstLng;
+	
+	console.log("test:");
+	console.log(req.query);
+	sg_func.searchRoute(srcLat,srcLng,dstLat,dstLng);
+	res.sendStaus(200);
 });
+
+router.get('/normalPath',function(req,res)
+{
+	var start_x = req.query.start_x;
+	var start_y = req.query.start_y;	
+	var des_x=req.query.des_x;
+	var des_y=req.query.des_y;
+	//sg_func.normalPath(start_x,start_y,des_x,des_y);
+	res.send(sg_func.normalPath(start_x,start_y,des_x,des_y));
+});
+
+router.get('/filterPath',function(req,res)
+{
+	var start_x = req.query.start_x;
+	var start_y = req.query.start_y;	
+	var des_x=req.query.des_x;
+	var des_y=req.query.des_y;
+	var year=req.query.year;
+	var month=req.query.month;
+	var hour=req.query.hour;
+	//sg_func.filterPath(start_x,start_y,des_x,des_y,year,month,hour);
+	res.send(sg_func.filterPath(start_x,start_y,des_x,des_y,year,month,hour));
+});
+
+
 
 module.exports = router;
